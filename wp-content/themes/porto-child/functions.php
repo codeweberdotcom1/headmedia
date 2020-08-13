a3b9838c056de72ec6a26bbaa314351b0cc8c364
Denis <web@dgladkov.ru>
2020-08-13 15:41:29 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -15,3 +15,39 @@ function porto_child_css() {
 		wp_enqueue_style( 'styles-child-rtl' );
 	}
 }
+
+
+
+<?php 
+
+
+
+
+function services($atts, $content = null){
+    ob_start();
+	
+global $porto_settings, $porto_layout, $post, $porto_member_socials;
+
+$featured_posts = get_field($atts['field']);
+		
+if( $featured_posts ): ?>
+    <ul class="porto-info-list block-ul-zw mb-0">
+    <?php  foreach( $featured_posts as $post ): 
+        // Setup this post for WP functions (variable must be named $post).
+        setup_postdata($post); 
+?>
+        <li class="porto-info-list-item"><i class="porto-info-icon fas fa-chevron-circle-right"></i>
+			<div class="porto-info-list-item-desc" font-size:="" 16px;="" style="font-size: 16px;"><a title="Перейти к услуге <?php the_title(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div></li>
+    <?php endforeach; ?>
+    </ul>
+    <?php 
+    // Reset the global post object so that the rest of the page works correctly.
+    wp_reset_postdata(); ?>
+<?php endif; ?>
+<?php
+    $output = ob_get_contents();
+    ob_end_clean(); 
+    return  $output;
+}?>
+<?php
+add_shortcode( 'service', 'services' );?>

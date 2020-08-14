e271ce88deb2347c0e8a0481c3c37948bd241fce
Denis <web@dgladkov.ru>
2020-08-14 11:45:37 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -82,3 +82,60 @@ if( $featured_posts ): ?>
 }?>
 <?php
 add_shortcode( 'service01', 'services01' );?>
+
+
+
+
+<?php function services02($atts, $content = null){
+    ob_start();
+	
+global $porto_settings, $porto_layout, $post, $porto_member_socials;
+
+$featured_posts = get_field($atts['field']);
+var_dump($featured_posts);
+		
+if( $featured_posts ): ?>
+<div class="container">
+	<div class="row">
+		<div class="col-2 d-none d-lg-block">
+			<img alt="Bootstrap Image Preview" src="https://via.placeholder.com/150" />
+		</div>
+		<div class="col-12 col-md-12 col-lg-10">
+			<div class="row">
+				<div class="col-4 col-md-2 d-block d-lg-none">
+					<img alt="Bootstrap Image Preview" src="https://via.placeholder.com/100" />
+				</div>
+				<div class="col-8 col-md-10 d-block title-block">
+					<h3 class="mb-0 mb-lg-3">Реклама в метро</h3>
+				</div>
+<div class="porto-separator col-12  mt-3 mt-lg-0"><hr class=" separator-line align_center solid" style="background-color:rgba(0,0,0,0.06);"></div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<ul class="ulli list-serviсes-hm list list-icons mb-0">
+						<?php  foreach( $featured_posts as $post ): 
+        // Setup this post for WP functions (variable must be named $post).
+        setup_postdata($post); 
+?>
+						<li class="mb-2 list-item">
+							<a class="text-secondary" title="Перейти к услуге <?php the_title(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+						</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php 
+    // Reset the global post object so that the rest of the page works correctly.
+    wp_reset_postdata(); ?>
+<?php endif; ?>
+<?php
+    $output = ob_get_contents();
+    ob_end_clean(); 
+    return  $output;
+}?>
+<?php
+add_shortcode( 'service02', 'services01' );?>
+

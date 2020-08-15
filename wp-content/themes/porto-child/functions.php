4fbf641e3b994ac753669231be9d3f713ad586b9
Denis <web@dgladkov.ru>
2020-08-15 19:32:43 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -137,3 +137,39 @@ if( $featured_posts ): ?>
 <?php
 add_shortcode( 'service02', 'services02' );?>
 
+
+
+
+
+<?php
+
+<?php function services03($atts, $content = null){
+    ob_start();
+	
+global $porto_settings, $porto_layout, $post, $porto_member_socials;
+
+$featured_posts = get_field($atts['field']);
+if( $featured_posts ): ?>
+    <ul>
+    <?php foreach( $featured_posts as $post ): 
+
+        // Setup this post for WP functions (variable must be named $post).
+        setup_postdata($post); ?>
+        <li>
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            <span>A custom field from this post: <?php the_field( 'field_name' ); ?></span>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+    <?php 
+    // Reset the global post object so that the rest of the page works correctly.
+    wp_reset_postdata(); ?>
+<?php endif; ?>
+<?php
+    $output = ob_get_contents();
+    ob_end_clean(); 
+    return  $output;
+}?>
+<?php
+add_shortcode( 'service03', 'services03' );?>
+

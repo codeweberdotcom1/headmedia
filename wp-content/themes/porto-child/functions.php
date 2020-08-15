c6a31ccdca4e4b2a449caae774e3cd6d1adfa30e
Denis <web@dgladkov.ru>
2020-08-15 22:13:57 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -147,12 +147,10 @@ add_shortcode( 'service02', 'services02' );?>
 global $porto_settings, $porto_layout, $post, $porto_member_socials;
 
 $featured_post = get_field($atts['field']);
+
 if( $featured_post ): ?>
     <h3><?php echo esc_html( $featured_post->post_title ); ?></h3>
 <?php endif; ?>
-if( $featured_post ): ?>
-    <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?><h3></a>
-<?php endif; 
     $output = ob_get_contents();
     ob_end_clean(); 
     return  $output;

e91f0d0f62824083f9fa0340c9cd701d85a61619
Denis <web@dgladkov.ru>
2020-08-15 19:21:01 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -104,7 +104,7 @@ if( $featured_posts ): ?>
 					<img alt="Bootstrap Image Preview" src="https://via.placeholder.com/100" />
 				</div>
 				<div class="col-8 col-md-10 d-block title-block">
-					<a href="#"><h3 class="mb-0 mb-lg-3"><?php echo $atts['text'] ?></h3></a>
+					<a href="#"><h3 class="mb-0 mb-lg-3"><?php echo $atts['title'] ?></h3></a>
 				</div>
 <div class="porto-separator col-12  mt-3 mt-lg-0"><hr class=" separator-line align_center solid" style="background-color:rgba(0,0,0,0.06);"></div>
 			</div>

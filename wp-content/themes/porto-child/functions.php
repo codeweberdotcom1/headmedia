af85895f8af6bb96de8bb36e670341b198a2ac71
Denis <web@dgladkov.ru>
2020-08-15 19:42:52 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -149,17 +149,17 @@ global $porto_settings, $porto_layout, $post, $porto_member_socials;
 
 $featured_posts = get_field($atts['field']);
 if( $featured_posts ): ?>
-    <ul>
+    
     <?php foreach( $featured_posts as $post ): 
 
         // Setup this post for WP functions (variable must be named $post).
         setup_postdata($post); ?>
-        <li>
+        
             <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
             
-        </li>
+       
     <?php endforeach; ?>
-    </ul>
+    
     <?php 
     // Reset the global post object so that the rest of the page works correctly.
     wp_reset_postdata(); ?>

6bf45f0cc8d863aba052d17da9373c59ad3e311b
Denis <web@dgladkov.ru>
2020-08-16 14:10:56 +0300

    Update taxonomy-category_press.php

--- wp-content/themes/porto-child/taxonomy-category_press.php
+++ wp-content/themes/porto-child/taxonomy-category_press.php
@@ -13,17 +13,23 @@ $member_options = get_metadata( $term->taxonomy, $term->term_id, 'member_options
 
 <?php get_header();?>
 
-
-
-<?php if (have_posts()) : while (have_posts()) : the_post();?>
-        <?php the_content();?>
-    <?php endwhile; endif;?>
-		<?php if ( category_description() ) : ?>
+<?php if ( category_description() ) : ?>
 		<div class="page-content">
-			
 			<?php echo category_description(); ?>
 		</div>
 	<?php endif; ?>
+
+	<div class="member-row members-container row <?php echo porto_generate_column_classes( $porto_settings['member-columns'] ); ?>">
+				<?php
+				while ( have_posts() ) {
+					the_post();
+					get_template_part( 'content', 'archive-member' );
+				}
+				?>
+			</div>
+			<?php porto_pagination(); ?>
+		</div>
+		<?php wp_reset_postdata(); ?>
 
 
 

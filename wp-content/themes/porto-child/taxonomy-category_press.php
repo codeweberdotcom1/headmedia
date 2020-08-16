3c590ef084283ebd62c126a2e111b8fd65bcae1d
Denis <web@dgladkov.ru>
2020-08-16 15:18:26 +0300

    Update taxonomy-category_press.php

--- wp-content/themes/porto-child/taxonomy-category_press.php
+++ wp-content/themes/porto-child/taxonomy-category_press.php
@@ -8,19 +8,33 @@ global $porto_settings, $porto_layout, $post;
 ?>
 <?php get_header();?>
 
+<?php
+// get the current taxonomy term
+$term = get_queried_object();
 
-<?php 
 
-$terms = get_queried_object();
-
-if( $terms && ! is_wp_error($terms) ){
-	echo "<ul>";
-	foreach( $terms as $term ){
-		echo "<li>". $term->name ."</li>";
-
-	}
-	echo "</ul>";
-}
+// vars
+$image = get_field('image', $term);
+$color = get_field('color', $term);
 
 ?>
+<style type="text/css">
+	
+	.entry-title a {
+		color: <?php echo $color; ?>;
+	}
+	
+	<?php if( $image ): ?>
+	.site-header {
+		background-image: url(<?php echo $image['url']; ?>);
+	}
+	<?php endif; ?>
+	
+</style>
+<div class="wrap">
+	
+	<?php // Remaining template removed from example ?>
+
+
+
 <?php get_footer();?>

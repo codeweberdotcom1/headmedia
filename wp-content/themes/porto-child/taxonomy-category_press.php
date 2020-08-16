615bce22aa0a207f90ebe8561e024b5866c02d6c
Denis <web@dgladkov.ru>
2020-08-16 14:21:38 +0300

    Update taxonomy-category_press.php

--- wp-content/themes/porto-child/taxonomy-category_press.php
+++ wp-content/themes/porto-child/taxonomy-category_press.php
@@ -4,38 +4,19 @@
  * Template Post Type: category_press
  */ 
 
-global $porto_settings, $wp_query;
-
-
+global $porto_settings, $porto_layout, $post;
 ?>
-
 <?php get_header();?>
 
-<?php
-$terms = get_terms(
-  array(
-    'taxonomy'   => 'my_taxonomy',
-    'hide_empty' => true,
-    'pad_counts'  => true,
-    'orderby' => 'count',
-    'order' => 'DESC',
-  )
-);
-  
-if ( ! empty( $terms ) && is_array( $terms ) ) {
-  echo '<ul class="list-my_taxonomy">';
-  foreach ( $terms as $term ) { ?>
-    <li>
-      <a href="<?php echo esc_url( get_term_link( $term ) ) ?>">
-        <?php echo $term->name; ?> (<?php echo $term->count; ?>)
-      </a>
-    </li>
-    <?php
-  }
-  echo '</ul>';
-}
-?>
 
+<?php $term = get_queried_object();?>
+
+<?php 
+   $tax = $wp_query->get_queried_object();
+   echo ''. $tax->name . '';
+   echo "<br>";
+   echo ''. $tax->description .''; 
+?>
 
 
 

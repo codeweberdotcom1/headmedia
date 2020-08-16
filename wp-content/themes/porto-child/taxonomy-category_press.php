7e6cd5b553368a088dc49aeeae29188e8f0cec49
Denis <web@dgladkov.ru>
2020-08-16 13:59:25 +0300

    Create taxonomy-category_press.php

---
+++ wp-content/themes/porto-child/taxonomy-category_press.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Template Name: My custom view
+ * Template Post Type: category_press
+ */ 
+
+global $porto_settings, $porto_layout, $post;
+?>
+<?php get_header();?>
+
+
+<?php $term = get_queried_object();?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post();?>
+        <?php the_content();?>
+    <?php endwhile; endif;?>
+		<?php if ( category_description() ) : ?>
+		<div class="page-content">
+			
+			<?php echo category_description(); ?>
+		</div>
+	<?php endif; ?>
+
+
+
+
+<?php get_footer();?>

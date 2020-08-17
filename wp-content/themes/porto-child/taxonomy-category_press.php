b39dc53de327484b893e5ce965319d86091f0bf0
Denis <web@dgladkov.ru>
2020-08-17 11:50:43 +0300

    Update taxonomy-category_press.php

--- wp-content/themes/porto-child/taxonomy-category_press.php
+++ wp-content/themes/porto-child/taxonomy-category_press.php
@@ -42,6 +42,35 @@ if( $image ):
 <?php endif; ?>
 
 
+<?php 
+
+$posts = get_posts(array(
+	'posts_per_page'	=> -1,
+	'post_type'			=> 'post'
+));
+
+if( $posts ): ?>
+	
+	<ul>
+		
+	<?php foreach( $posts as $post ): 
+		
+		setup_postdata( $post );
+		
+		?>
+		<li>
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</li>
+	
+	<?php endforeach; ?>
+	
+	</ul>
+	
+	<?php wp_reset_postdata(); ?>
+
+<?php endif; ?>
+
+
 
 
 

1060515ff345e4b9d1f6e0e8d058cdbf28b1d0ee
Denis <web@dgladkov.ru>
2020-08-17 11:18:59 +0300

    Update taxonomy-category_press.php

--- wp-content/themes/porto-child/taxonomy-category_press.php
+++ wp-content/themes/porto-child/taxonomy-category_press.php
@@ -12,22 +12,7 @@ global $porto_settings, $porto_layout, $post;
     <img src="<?php the_field('image_posts'); ?>" />
 <?php endif; ?>
 
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-6">
-			<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" />
-		</div>
-		<div class="col-md-6">
-			<p>
-				<?php if ( category_description() ) : ?>
-		<div class="page-content">
-			<?php echo category_description(); ?>
-		</div>
-	<?php endif; ?>
-			</p>
-		</div>
-	</div>
-</div>
+
 
 
 

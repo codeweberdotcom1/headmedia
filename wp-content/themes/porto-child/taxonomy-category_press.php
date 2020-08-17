100d7e5c520949bc2c1e5b86820a281ec4aec128
Denis <web@dgladkov.ru>
2020-08-17 11:24:54 +0300

    Update taxonomy-category_press.php

--- wp-content/themes/porto-child/taxonomy-category_press.php
+++ wp-content/themes/porto-child/taxonomy-category_press.php
@@ -7,8 +7,37 @@
 global $porto_settings, $porto_layout, $post; $term?>
 <?php get_header();?>
 
-<?php if( get_field('image_posts') ): ?>
-    <img src="<?php get_field('image_posts'); ?>" />
+<?php
+$image = get_field('image_posts');
+if( $image ):
+
+    // Image variables.
+    $url = $image['url'];
+    $title = $image['title'];
+    $alt = $image['alt'];
+    $caption = $image['caption'];
+
+    // Thumbnail size attributes.
+    $size = 'thumbnail';
+    $thumb = $image['sizes'][ $size ];
+    $width = $image['sizes'][ $size . '-width' ];
+    $height = $image['sizes'][ $size . '-height' ];
+
+    // Begin caption wrap.
+    if( $caption ): ?>
+        <div class="wp-caption">
+    <?php endif; ?>
+
+    <a href="<?php echo esc_url($url); ?>" title="<?php echo esc_attr($title); ?>">
+        <img src="<?php echo esc_url($thumb); ?>" alt="<?php echo esc_attr($alt); ?>" />
+    </a>
+
+    <?php 
+    // End caption wrap.
+    if( $caption ): ?>
+        <p class="wp-caption-text"><?php echo esc_html($caption); ?></p>
+        </div>
+    <?php endif; ?>
 <?php endif; ?>
 
 

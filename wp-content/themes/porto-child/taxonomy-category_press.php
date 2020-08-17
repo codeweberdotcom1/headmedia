662efb139b5335bece777d3c6b837744c4593af5
Denis <web@dgladkov.ru>
2020-08-17 11:52:36 +0300

    Update taxonomy-category_press.php

--- wp-content/themes/porto-child/taxonomy-category_press.php
+++ wp-content/themes/porto-child/taxonomy-category_press.php
@@ -4,74 +4,33 @@
  * Template Post Type: category_press
  */ 
 
-global $porto_settings, $porto_layout, $post; $term?>
+global $porto_settings, $porto_layout, $post; ?>
 <?php get_header();?>
 
-<?php
-$image = get_field('imagepress');
-var_dump( $image );
-if( $image ):
-
-    // Image variables.
-    $url = $image['url'];
-    $title = $image['title'];
-    $alt = $image['alt'];
-    $caption = $image['caption'];
-
-    // Thumbnail size attributes.
-    $size = 'thumbnail';
-    $thumb = $image['sizes'][ $size ];
-    $width = $image['sizes'][ $size . '-width' ];
-    $height = $image['sizes'][ $size . '-height' ];
-
-    // Begin caption wrap.
-    if( $caption ): ?>
-        <div class="wp-caption">
-    <?php endif; ?>
-
-    <a href="<?php echo esc_url($url); ?>" title="<?php echo esc_attr($title); ?>">
-        <img src="<?php echo esc_url($thumb); ?>" alt="<?php echo esc_attr($alt); ?>" />
-    </a>
-
-    <?php 
-    // End caption wrap.
-    if( $caption ): ?>
-        <p class="wp-caption-text"><?php echo esc_html($caption); ?></p>
-        </div>
-    <?php endif; ?>
+<?php if( get_field('image_posts') ): ?>
+    <img src="<?php get_field('image_posts'); ?>" />
+<?php endif; ?>
+<?php if( get_field('tekst') ): ?>
+    <p><?php get_field('tekst'); ?>" </p>
 <?php endif; ?>
 
 
-<?php 
-
-$posts = get_posts(array(
-	'posts_per_page'	=> -1,
-	'post_type'			=> 'post'
-));
-
-if( $posts ): ?>
-	
-	<ul>
-		
-	<?php foreach( $posts as $post ): 
-		
-		setup_postdata( $post );
-		
-		?>
-		<li>
-			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-		</li>
-	
-	<?php endforeach; ?>
-	
-	</ul>
-	
-	<?php wp_reset_postdata(); ?>
-
-<?php endif; ?>
-
-
-
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6">
+			<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg" />
+		</div>
+		<div class="col-md-6">
+			<p>
+				<?php if ( category_description() ) : ?>
+		<div class="page-content">
+			<?php echo category_description(); ?>
+		</div>
+	<?php endif; ?>
+			</p>
+		</div>
+	</div>
+</div>
 
 
 

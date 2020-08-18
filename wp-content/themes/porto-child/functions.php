b20dee592ae69771acaa7cd3a8380446133b22c0
Denis <web@dgladkov.ru>
2020-08-19 00:55:13 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -391,6 +391,7 @@ add_shortcode( 'subservices1', 'subservices1' );?>
         // Setup this post for WP functions (variable must be named $post).
 												setup_postdata($post); 
 												?>
+										<?php 	var_dump($post)	?>
 				<div class="vc_column_container col-md-12 appear-animation fadeInUp appear-animation-visible" data-appear-animation="fadeInUp"><div class="wpb_wrapper vc_column-inner">
 					
 						<div class="wpb_text_column wpb_content_element p-3 mb-3 shadow-hm-zw bg-light">

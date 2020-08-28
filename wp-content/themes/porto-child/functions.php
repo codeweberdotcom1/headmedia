9002ec5611b1ffd65820ce66ef2bd1973a99c57a
Denis <web@dgladkov.ru>
2020-08-28 14:37:51 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -420,7 +420,7 @@ add_shortcode( 'subservices1', 'subservices1' );?>
 											</div>
 											<div class="col-8 col-md-8 col-lg-12 d-block title-block"><h3 style="font-size: 18px" class="mb-0  text-uppercase"><?php the_title(); ?></h3>
 											</div>
-<div class="col-1 col-md-1 col-lg-1 align-self-center">
+<div class="col-1 p-0 col-md-1 col-lg-1 align-self-center">
 											
 											<i title="Перейти к услуге <?php the_title(); ?>" class=" red-tooltip view-more  Simple-Line-Icons-arrow-right-circle font-weight-semibold"></i>
 											

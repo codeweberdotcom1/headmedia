56e08b7b0191fb1dc07351dcee603dfb704874f7
Denis <web@dgladkov.ru>
2020-08-28 14:08:45 +0300

    Update functions.php

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -418,12 +418,13 @@ add_shortcode( 'subservices1', 'subservices1' );?>
 													<div class="row">
 														<div class="col-4 col-md-4 d-block d-lg-none"><?php the_post_thumbnail('widget-thumb-medium'); ?>
 													</div>
-													<div class="col-8 col-md-8 col-lg-12 d-block title-block"><h3 style="font-size: 18px" class="mb-0  text-uppercase"><?php the_title(); ?></h3><i title="Перейти к услуге <?php the_title(); ?>" class="red-tooltip view-more Simple-Line-Icons-arrow-right-circle font-weight-semibold"></i>
+													<div class="col-8 col-md-8 col-lg-12 d-block title-block"><h3 style="font-size: 18px" class="mb-0  text-uppercase"><?php the_title(); ?></h3>
 													</div>
 												</div>
 												
 											
 										</div>
+										<i title="Перейти к услуге <?php the_title(); ?>" class="red-tooltip view-more Simple-Line-Icons-arrow-right-circle font-weight-semibold"></i>
 									</div>
 								</div>
 							</div>
